a9e934edb8fa4888a4dbfe02af990f471d3976e9
jorge.quiros.torres <jorge.quiros.torres@outlook.com>
2019-02-15 10:51:04 -0600

    Trying to add to cucumber branch

--- Test/pages/HomePage.ts
+++ Test/pages/HomePage.ts
@@ -1,7 +1,6 @@
 //from protractor   
 import { browser, element, by, $$, $ } from "protractor";
-import { BasePage, IdentificationType } from "./BasePage";
-
+import { BasePage, IdentificationType } from "./BasePage";  
 // const using json format, this is like page factory in Java, applying same concept
 const Locators = {
     heading : {
